d28e90c4230e25ad9e2393e4511251161530b02b
bimosyah <bimosyahputro88@gmail.com>
2019-11-06 04:09:18 +0700

    fix halaman pengguna

--- app/src/main/java/syahputro/bimo/projek/dinas/p3a/fragment/FragmentPengguna.java
+++ app/src/main/java/syahputro/bimo/projek/dinas/p3a/fragment/FragmentPengguna.java
@@ -1,13 +1,20 @@
 package syahputro.bimo.projek.dinas.p3a.fragment;
 
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import syahputro.bimo.projek.dinas.p3a.R;
 
@@ -15,7 +22,7 @@ import syahputro.bimo.projek.dinas.p3a.R;
  * A simple {@link Fragment} subclass.
  */
 public class FragmentPengguna extends Fragment {
-
+    TextView tvCall, tvKonsul;
 
     public FragmentPengguna() {
         // Required empty public constructor
@@ -30,4 +37,35 @@ public class FragmentPengguna extends Fragment {
         return inflater.inflate(R.layout.fragment_pengguna, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tvCall = view.findViewById(R.id.tvCall);
+        tvKonsul = view.findViewById(R.id.tvKonsul);
+
+        tvKonsul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    String mobile = "+6281233045596";
+                    String msg = "Its Working";
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://api.whatsapp.com/send?phone=" + mobile + "&text=" + msg)));
+                }catch (Exception e){
+                    //whatsapp app not install
+                }
+            }
+        });
+
+        tvCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mobileNumber = "081233045596";
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_DIAL); // Action for what intent called for
+                intent.setData(Uri.parse("tel: " + mobileNumber)); // Data with intent respective action on intent
+                startActivity(intent);
+            }
+        });
+    }
 }
