3b090e1a96d40025525c2ee4975d127e8b1efa6f
bimosyah <bimosyahputro88@gmail.com>
2019-11-25 21:52:33 +0700

    statistik

--- app/src/main/java/syahputro/bimo/projek/dinas/p3a/fragment/FragmentStatistik.java
+++ app/src/main/java/syahputro/bimo/projek/dinas/p3a/fragment/FragmentStatistik.java
@@ -3,11 +3,22 @@ package syahputro.bimo.projek.dinas.p3a.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import syahputro.bimo.projek.dinas.p3a.R;
 
@@ -30,4 +41,20 @@ public class FragmentStatistik extends Fragment {
         return inflater.inflate(R.layout.fragment_statistik, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Pie pie = AnyChart.pie();
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("John", 10000));
+        data.add(new ValueDataEntry("Jake", 12000));
+        data.add(new ValueDataEntry("Peter", 18000));
+
+        pie.data(data);
+
+        AnyChartView anyChartView = view.findViewById(R.id.any_chart_view);
+        anyChartView.setChart(pie);
+
+    }
 }
