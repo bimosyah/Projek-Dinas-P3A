62e8be2fef6ae4658c76037ec47fb4166959304d
bimosyah <bimosyahputro88@gmail.com>
2019-12-03 23:01:48 +0700

    file layout baru

---
+++ app/src/main/java/syahputro/bimo/projek/dinas/p3a/activity/activity_baru/ActivityProfilDinas.java
@@ -0,0 +1,16 @@
+package syahputro.bimo.projek.dinas.p3a.activity.activity_baru;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import syahputro.bimo.projek.dinas.p3a.R;
+
+public class ActivityProfilDinas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil_dinas);
+    }
+}
---
+++ app/src/main/java/syahputro/bimo/projek/dinas/p3a/activity/activity_baru/ActivityStatistik.java
@@ -0,0 +1,16 @@
+package syahputro.bimo.projek.dinas.p3a.activity.activity_baru;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import syahputro.bimo.projek.dinas.p3a.R;
+
+public class ActivityStatistik extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistik);
+    }
+}
