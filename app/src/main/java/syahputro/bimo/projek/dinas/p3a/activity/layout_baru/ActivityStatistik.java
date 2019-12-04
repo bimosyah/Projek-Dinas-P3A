ae145091e3c7832e113a1cf1e26c0326cfd8d0c9
bimosyah <bimosyahputro88@gmail.com>
2019-12-04 08:48:44 +0700

    fix menu statistik dan profile dinas

--- app/src/main/java/syahputro/bimo/projek/dinas/p3a/activity/layout_baru/ActivityStatistik.java
+++ app/src/main/java/syahputro/bimo/projek/dinas/p3a/activity/layout_baru/ActivityStatistik.java
@@ -4,6 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import syahputro.bimo.projek.dinas.p3a.R;
 
 public class ActivityStatistik extends AppCompatActivity {
@@ -14,6 +23,18 @@ public class ActivityStatistik extends AppCompatActivity {
         setContentView(R.layout.activity_statistik);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Statistik");
+
+        Pie pie = AnyChart.pie();
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("John", 10000));
+        data.add(new ValueDataEntry("Jake", 12000));
+        data.add(new ValueDataEntry("Peter", 18000));
+
+        pie.data(data);
+
+        AnyChartView anyChartView = findViewById(R.id.any_chart_view);
+        anyChartView.setChart(pie);
     }
 
     @Override
