af9b8dadc8744d887a6da4dab8f8d31dd06f3fd6
bimosyah <bimosyahputro88@gmail.com>
2019-12-10 23:21:16 +0700

    bantuan dan lupa password

--- app/src/main/java/syahputro/bimo/projek/dinas/p3a/activity/ActivityBantuan.java
+++ app/src/main/java/syahputro/bimo/projek/dinas/p3a/activity/ActivityBantuan.java
@@ -3,10 +3,12 @@ package syahputro.bimo.projek.dinas.p3a.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import syahputro.bimo.projek.dinas.p3a.R;
 
 public class ActivityBantuan extends AppCompatActivity {
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +16,13 @@ public class ActivityBantuan extends AppCompatActivity {
         setContentView(R.layout.activity_bantuan);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Bantuan");
+
+        init();
+        webView.loadUrl("http://dp3akabupatenmalang.com/dp3a/api/info/bantuan");
+    }
+
+    private void init(){
+        webView = findViewById(R.id.wvBantuan);
     }
 
     @Override
--- app/src/main/java/syahputro/bimo/projek/dinas/p3a/activity/ActivityLupaSandi.java
+++ app/src/main/java/syahputro/bimo/projek/dinas/p3a/activity/ActivityLupaSandi.java
@@ -3,10 +3,12 @@ package syahputro.bimo.projek.dinas.p3a.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import syahputro.bimo.projek.dinas.p3a.R;
 
 public class ActivityLupaSandi extends AppCompatActivity {
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +16,14 @@ public class ActivityLupaSandi extends AppCompatActivity {
         setContentView(R.layout.activity_lupa_sandi);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Lupa Kata Sandi");
+
+        init();
+        webView.loadUrl("http://dp3akabupatenmalang.com/dp3a/api/info/lupa");
+
+    }
+
+    private void init(){
+        webView = findViewById(R.id.wvLupaPassword);
     }
 
     @Override
