57ec23ca9905375c9924c76cb9a4e2d0bb2809b8
bimosyah <bimosyahputro88@gmail.com>
2020-02-19 19:52:14 +0700

    v5 hapus kordinat

--- app/src/main/java/syahputro/bimo/projek/dinas/p3a/activity/ActivityPengaduan.java
+++ app/src/main/java/syahputro/bimo/projek/dinas/p3a/activity/ActivityPengaduan.java
@@ -1,32 +1,20 @@
 package syahputro.bimo.projek.dinas.p3a.activity;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import com.kusu.loadingbutton.LoadingButton;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -42,16 +30,13 @@ import syahputro.bimo.projek.dinas.p3a.network.response.pengaduan.ResponsePengad
 import syahputro.bimo.projek.dinas.p3a.utils.Preference;
 
 public class ActivityPengaduan extends AppCompatActivity {
-    private static final int REQUEST_LOCATION = 1;
     private final ArrayList<String> array_kategori = new ArrayList<String>();
     private final ArrayList<String> array_kecamatan = new ArrayList<String>();
     private final ArrayList<String> array_id_kecamatan = new ArrayList<String>();
     private final ArrayList<String> array_id_kategori = new ArrayList<String>();
     private ApiService service;
-    private Button btn_submit;
     private Spinner spinner_kategori, spinner_jenkel, spinner_kecamatan;
     private EditText et_pengaduan, et_desa, et_dusun, et_usia;
-    private LocationManager locationManager;
     private String longitude = "", latitude = "";
     private LoadingButton loadingButton;
     private int id_kategori;
@@ -122,7 +107,6 @@ public class ActivityPengaduan extends AppCompatActivity {
                 }
             }
         });
-
     }
 
     private void loadKategori() {
@@ -189,60 +173,44 @@ public class ActivityPengaduan extends AppCompatActivity {
         loadingButton = findViewById(R.id.loadingButton);
     }
 
-    private void gpsCheck() {
-        locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
-        if (!Objects.requireNonNull(locationManager).isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            onGPS();
-        } else {
-            getLocation();
-        }
-    }
-
     private void savePengaduan() {
-        gpsCheck();
-
-        if (!latitude.equals("") && !longitude.equals("")) {
-            String id_user = Preference.getIdUser(getApplicationContext());
-            Call<ResponsePengaduan> pengaduan = service.pengaduan(
-                    Integer.parseInt(id_user),
-                    id_kategori,
-                    et_pengaduan.getText().toString(),
-                    jenkel,
-                    Integer.parseInt(et_usia.getText().toString()),
-                    id_kecamatan,
-                    et_desa.getText().toString(),
-                    et_dusun.getText().toString(),
-                    latitude,
-                    longitude);
-            pengaduan.enqueue(new Callback<ResponsePengaduan>() {
-                @Override
-                public void onResponse(Call<ResponsePengaduan> call, Response<ResponsePengaduan> response) {
-                    if (response.isSuccessful()) {
-                        if (response.body() != null) {
-                            if (response.body().getStatus().equals("0")) {
-                                loadingButton.hideLoading();
-                                Intent intent = new Intent(ActivityPengaduan.this, ActivityRiwayat.class);
-                                intent.putExtra("EXIT", true);
-                                startActivity(intent);
-                                Toast.makeText(getApplicationContext(), "Pengaduan Tersimpan", Toast.LENGTH_LONG).show();
-                            } else if (response.body().getStatus().equals("1")) {
-                                loadingButton.hideLoading();
-                                Toast.makeText(getApplicationContext(), response.body().getMessage(), Toast.LENGTH_LONG).show();
-                            }
+        String id_user = Preference.getIdUser(getApplicationContext());
+        Call<ResponsePengaduan> pengaduan = service.pengaduan(
+                Integer.parseInt(id_user),
+                id_kategori,
+                et_pengaduan.getText().toString(),
+                jenkel,
+                Integer.parseInt(et_usia.getText().toString()),
+                id_kecamatan,
+                et_desa.getText().toString(),
+                et_dusun.getText().toString(),
+                latitude,
+                longitude);
+        pengaduan.enqueue(new Callback<ResponsePengaduan>() {
+            @Override
+            public void onResponse(Call<ResponsePengaduan> call, Response<ResponsePengaduan> response) {
+                if (response.isSuccessful()) {
+                    if (response.body() != null) {
+                        if (response.body().getStatus().equals("0")) {
+                            loadingButton.hideLoading();
+                            Intent intent = new Intent(ActivityPengaduan.this, ActivityRiwayat.class);
+                            intent.putExtra("EXIT", true);
+                            startActivity(intent);
+                            Toast.makeText(getApplicationContext(), "Pengaduan Tersimpan", Toast.LENGTH_LONG).show();
+                        } else if (response.body().getStatus().equals("1")) {
+                            loadingButton.hideLoading();
+                            Toast.makeText(getApplicationContext(), response.body().getMessage(), Toast.LENGTH_LONG).show();
                         }
                     }
                 }
+            }
 
-                @Override
-                public void onFailure(Call<ResponsePengaduan> call, Throwable t) {
-                    loadingButton.hideLoading();
-                    Toast.makeText(getApplicationContext(), "error " + t.getMessage(), Toast.LENGTH_LONG).show();
-                }
-            });
-        } else {
-            gpsCheck();
-            loadingButton.hideLoading();
-        }
+            @Override
+            public void onFailure(Call<ResponsePengaduan> call, Throwable t) {
+                loadingButton.hideLoading();
+                Toast.makeText(getApplicationContext(), "error " + t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 
@@ -263,56 +231,6 @@ public class ActivityPengaduan extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, items);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner_jenkel.setAdapter(adapter);
-    }
-
-    private void onGPS() {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
-        builder.setMessage("Enable GPS").setCancelable(false).setPositiveButton("YES", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-            }
-        }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.cancel();
-            }
-        });
-        final AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-    }
-
-    private void getLocation() {
-        //check permission again
-        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(),
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]
-                    {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
-        } else {
-            Location LocationGps = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            Location LocationNetwork = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-            Location LocationPassive = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
-
-            if (LocationGps != null) {
-                double lat = LocationGps.getLatitude();
-                double longi = LocationGps.getLongitude();
-                latitude = String.valueOf(lat);
-                longitude = String.valueOf(longi);
-            } else if (LocationNetwork != null) {
-                double lat = LocationNetwork.getLatitude();
-                double longi = LocationNetwork.getLongitude();
-                latitude = String.valueOf(lat);
-                longitude = String.valueOf(longi);
-            } else if (LocationPassive != null) {
-                double lat = LocationPassive.getLatitude();
-                double longi = LocationPassive.getLongitude();
-                latitude = String.valueOf(lat);
-                longitude = String.valueOf(longi);
-            } else {
-                Log.d("LOCATION", "getLocation: error");
-            }
-        }
     }
 
     @Override
--- app/src/main/java/syahputro/bimo/projek/dinas/p3a/activity/ActivityRegister.java
+++ app/src/main/java/syahputro/bimo/projek/dinas/p3a/activity/ActivityRegister.java
@@ -1,30 +1,19 @@
 package syahputro.bimo.projek.dinas.p3a.activity;
 
-import android.Manifest;
 import android.app.DatePickerDialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import com.kusu.loadingbutton.LoadingButton;
 
 import java.util.Calendar;
-import java.util.Objects;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -35,9 +24,7 @@ import syahputro.bimo.projek.dinas.p3a.network.ApiService;
 import syahputro.bimo.projek.dinas.p3a.network.response.register.ResponseRegister;
 
 public class ActivityRegister extends AppCompatActivity {
-    private static final int REQUEST_LOCATION = 1;
     Button btn_register;
-    LocationManager locationManager;
     String longitude = "", latitude = "";
     EditText etNama, etPassword, etNoTelp, etAlamat;
     EditText etTanggalLahir;
@@ -50,10 +37,6 @@ public class ActivityRegister extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         init();
         service = ApiClient.getClient().create(ApiService.class);
-
-        //add permission
-        ActivityCompat.requestPermissions(this, new String[]
-                {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
 
         etTanggalLahir.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
@@ -87,89 +70,48 @@ public class ActivityRegister extends AppCompatActivity {
         loadingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                gpsCheck();
-
-                if (!latitude.equals("") && !longitude.equals("")){
-                    loadingButton.showLoading();
-                    String nama = etNama.getText().toString();
-                    String password = etPassword.getText().toString();
-                    String notelp = etNoTelp.getText().toString();
-                    String alamat = etAlamat.getText().toString();
-                    String tglLahir = etTanggalLahir.getText().toString();
-                    if (nama.equals("") || password.equals("") || notelp.equals("") || alamat.equals("") || tglLahir.equals("")) {
-                        Toast.makeText(getApplicationContext(), "Semua form harus diisi", Toast.LENGTH_LONG).show();
-                        loadingButton.hideLoading();
-                    } else {
-                        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-                        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-                            onGPS();
-                        } else {
-                            getLocation();
-                        }
-
-                        Call<ResponseRegister> registrasi = service.register(
-                                nama,
-                                password,
-                                notelp,
-                                alamat,
-                                tglLahir,
-                                latitude,
-                                longitude
-                        );
-                        registrasi.enqueue(new Callback<ResponseRegister>() {
-                            @Override
-                            public void onResponse(Call<ResponseRegister> call, Response<ResponseRegister> response) {
-                                if (response.isSuccessful()) { // check response suksess or no
-                                    if (response.body() != null) {
-                                        loadingButton.hideLoading();
-                                        Toast.makeText(getApplicationContext(), response.body().getMessage(), Toast.LENGTH_LONG).show();
-                                        if (!response.body().getStatus().equals("1")) {
-                                            startActivity(new Intent(ActivityRegister.this, ActivityLogin.class));
-                                        }
+                loadingButton.showLoading();
+                String nama = etNama.getText().toString();
+                String password = etPassword.getText().toString();
+                String notelp = etNoTelp.getText().toString();
+                String alamat = etAlamat.getText().toString();
+                String tglLahir = etTanggalLahir.getText().toString();
+                if (nama.equals("") || password.equals("") || notelp.equals("") || alamat.equals("") || tglLahir.equals("")) {
+                    Toast.makeText(getApplicationContext(), "Semua form harus diisi", Toast.LENGTH_LONG).show();
+                    loadingButton.hideLoading();
+                } else {
+                    Call<ResponseRegister> registrasi = service.register(
+                            nama,
+                            password,
+                            notelp,
+                            alamat,
+                            tglLahir,
+                            latitude,
+                            longitude
+                    );
+                    registrasi.enqueue(new Callback<ResponseRegister>() {
+                        @Override
+                        public void onResponse(Call<ResponseRegister> call, Response<ResponseRegister> response) {
+                            if (response.isSuccessful()) { // check response suksess or no
+                                if (response.body() != null) {
+                                    loadingButton.hideLoading();
+                                    Toast.makeText(getApplicationContext(), response.body().getMessage(), Toast.LENGTH_LONG).show();
+                                    if (!response.body().getStatus().equals("1")) {
+                                        startActivity(new Intent(ActivityRegister.this, ActivityLogin.class));
                                     }
                                 }
                             }
+                        }
 
-                            @Override
-                            public void onFailure(Call<ResponseRegister> call, Throwable t) {
-                                loadingButton.hideLoading();
-                                Toast.makeText(getApplicationContext(), "error " + t.getMessage(), Toast.LENGTH_LONG).show();
-                            }
-                        });
-                    }
-                }else {
-                    gpsCheck();
-                    loadingButton.hideLoading();
+                        @Override
+                        public void onFailure(Call<ResponseRegister> call, Throwable t) {
+                            loadingButton.hideLoading();
+                            Toast.makeText(getApplicationContext(), "error " + t.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    });
                 }
-
             }
         });
-    }
-
-    private void gpsCheck() {
-        locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
-        if (!Objects.requireNonNull(locationManager).isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            onGPS();
-        } else {
-            getLocation();
-        }
-    }
-
-    private void onGPS() {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Enable GPS").setCancelable(false).setPositiveButton("YES", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-            }
-        }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.cancel();
-            }
-        });
-        final AlertDialog alertDialog = builder.create();
-        alertDialog.show();
     }
 
     private void init() {
@@ -181,38 +123,5 @@ public class ActivityRegister extends AppCompatActivity {
         etTanggalLahir = findViewById(R.id.etTanggalLahir);
         loadingButton = findViewById(R.id.loadingButton);
 
-    }
-
-    private void getLocation() {
-        //check permission again
-        if (ActivityCompat.checkSelfPermission(ActivityRegister.this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(ActivityRegister.this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]
-                    {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
-        } else {
-            Location LocationGps = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            Location LocationNetwork = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-            Location LocationPassive = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
-
-            if (LocationGps != null) {
-                double lat = LocationGps.getLatitude();
-                double longi = LocationGps.getLongitude();
-                latitude = String.valueOf(lat);
-                longitude = String.valueOf(longi);
-            } else if (LocationNetwork != null) {
-                double lat = LocationNetwork.getLatitude();
-                double longi = LocationNetwork.getLongitude();
-                latitude = String.valueOf(lat);
-                longitude = String.valueOf(longi);
-            } else if (LocationPassive != null) {
-                double lat = LocationPassive.getLatitude();
-                double longi = LocationPassive.getLongitude();
-                latitude = String.valueOf(lat);
-                longitude = String.valueOf(longi);
-            } else {
-                Log.d("LOCATION", "getLocation: error");
-            }
-        }
     }
 }
